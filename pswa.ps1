3e14d121a4eac624563269f5b2e77f6651010e7f
kxclark <36680633+kxclark@users.noreply.github.com>
2018-02-20 15:35:02 -0800

    first pwsa script

---
+++ pswa.ps1
@@ -0,0 +1,5 @@
+﻿### Install PSWA Components ###
+
+Install-WindowsFeature -Name "WindowsPowerShellWebAccess" -IncludeAllSubFeature -IncludeManagementTools -Restart
+Install-PswaWebApplication -UseTestCertificate
+Add-PswaAuthorizationRule -UserGroupName * -ComputerGroupName * -ConfigurationName *
