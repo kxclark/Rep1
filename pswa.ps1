e0384c2e2a1b1a41642d7662e4902590f2bd733e
kxclark <36680633+kxclark@users.noreply.github.com>
2018-02-23 09:51:22 -0800

    added powershell env line

--- pswa.ps1
+++ pswa.ps1
@@ -1,6 +1,8 @@
 ﻿### Install PSWA Components ###
 ## This is a test ##
 
+#!/usr/bin/env powershell
+
 Install-WindowsFeature -Name "WindowsPowerShellWebAccess" -IncludeAllSubFeature -IncludeManagementTools -Restart
 Install-PswaWebApplication -UseTestCertificate
 Add-PswaAuthorizationRule -UserGroupName * -ComputerGroupName * -ConfigurationName *
