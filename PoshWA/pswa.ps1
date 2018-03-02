f77da4d375bc03443ffde4cafe6d49d59dc09c69
kxclark <36680633+kxclark@users.noreply.github.com>
2018-03-02 14:51:27 -0800

    added poshwa folder and moved script to folder

---
+++ PoshWA/pswa.ps1
@@ -0,0 +1,25 @@
+﻿### Install PSWA Components ###
+## This is a test ##
+
+#!/usr/bin/env powershell
+
+
+#Install Powershell Web Access Feature
+Function pswainst{
+    If(!(Get-WindowsFeature -Name WindowsPowerShellWebAccess).installed)
+    {
+        Install-WindowsFeature -Name "WindowsPowerShellWebAccess" -IncludeAllSubFeature -IncludeManagementTools -Restart
+    }
+}
+
+#Settings - install pswa web app, add auth rule, disable firewalls
+Function settings{    
+    Install-PswaWebApplication -UseTestCertificate
+    Add-PswaAuthorizationRule -UserGroupName * -ComputerGroupName * -ConfigurationName *
+    Set-NetFirewallProfile -All -Enabled False
+}
+
+## Call Functions ##
+pswainst
+settings
+Exit 0
