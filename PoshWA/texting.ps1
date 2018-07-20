481067caa3decbd6e6120846f6641b8fddaad9eb
kxclark <36680633+kxclark@users.noreply.github.com>
2018-07-20 10:32:48 -0700

    added if

--- PoshWA/texting.ps1
+++ PoshWA/texting.ps1
@@ -1,2 +1,12 @@
-﻿New-Item -Name neww.txt -Path c:\tmp -ItemType "file"
-Add-Content -Path c:\tmp\neww.txt -Value "this is a "%loginName% %password%
+﻿param(
+        [string]
+        $login,
+        [string]
+        $pass
+      )
+
+if(!Get-ChildItem c:\tmp\neww.txt)
+{
+New-Item -Name neww.txt -Path c:\tmp -ItemType "file"
+}
+Add-Content -Path c:\tmp\neww.txt -Value "this is a $login and $pass"
