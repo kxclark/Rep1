9c2288dae1422588eb34365eba06e581365f45c7
kxclark <36680633+kxclark@users.noreply.github.com>
2018-07-20 11:50:55 -0700

    added log

--- PoshWA/texting.ps1
+++ PoshWA/texting.ps1
@@ -10,5 +10,7 @@
 New-Item -Name neww.txt -Path c:\tmp -ItemType "file"
 #}
 sleep 5
+
+Write-Verbose "running add content"
 Add-Content -Path "c:\tmp\neww.txt" -Value "Line 1 add content works!"
 Add-Content -Path "c:\tmp\neww.txt" -Value "this is a $login and $pass"
