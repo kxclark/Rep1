6c50b3c632b5966246771cdc263b775d30ee1500
kxclark <36680633+kxclark@users.noreply.github.com>
2018-07-20 11:10:24 -0700

    rem if

--- PoshWA/texting.ps1
+++ PoshWA/texting.ps1
@@ -5,8 +5,8 @@
         $pass
       )
 
-if(!Get-ChildItem c:\tmp\neww.txt)
-{
+#if(!Get-ChildItem c:\tmp\neww.txt)
+#{
 New-Item -Name neww.txt -Path c:\tmp -ItemType "file"
-}
+#}
 Add-Content -Path c:\tmp\neww.txt -Value "this is a $login and $pass"
